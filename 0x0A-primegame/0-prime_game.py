ed9df3017bebb120e6e2a99056c1b4ff6813640d
mooka007 <mooka3084@gmail.com>
2024-05-17 08:09:15 -0700

    srotate 2d

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,57 +1,41 @@
 #!/usr/bin/python3
-"""Module defining isWinner function."""
+"""
+The player that cannot make a move loses the game.
+"""
 
 
 def isWinner(x, nums):
-    """Function to get who has won in prime game"""
-    mariaWinsCount = 0
-    benWinsCount = 0
-
-    for num in nums:
-        roundsSet = list(range(1, num + 1))
-        primesSet = primes_in_range(1, num)
-
-        if not primesSet:
-            benWinsCount += 1
-            continue
-
-        isMariaTurns = True
-
-        while(True):
-            if not primesSet:
-                if isMariaTurns:
-                    benWinsCount += 1
-                else:
-                    mariaWinsCount += 1
-                break
-
-            smallestPrime = primesSet.pop(0)
-            roundsSet.remove(smallestPrime)
-
-            roundsSet = [x for x in roundsSet if x % smallestPrime != 0]
-
-            isMariaTurns = not isMariaTurns
-
-    if mariaWinsCount > benWinsCount:
-        return "Winner: Maria"
-
-    if mariaWinsCount < benWinsCount:
-        return "Winner: Ben"
-
-    return None
-
-
-def is_prime(n):
-    """Returns True if n is prime, else False."""
-    if n < 2:
-        return False
-    for i in range(2, int(n ** 0.5) + 1):
-        if n % i == 0:
+    """
+    Return: name of the player that won the most rounds.
+    If the winner cannot be determined, return `None`.
+    """
+    def is_prime(n):
+        """"""
+        remainders = []
+        if (n <= 1):
             return False
-    return True
+        if (n <= 3):
+            return True
+        if (n % 2 == 0 or n % 3 == 0):
+            return False
+        i = 5
+        while(i * i <= n):
+            if (n % i == 0 or n % (i + 2) == 0):
+                return False
+            i = i + 6
+        return True
 
+    nums.sort()
 
-def primes_in_range(start, end):
-    """Returns a list of prime numbers between start and end (inclusive)."""
-    primes = [n for n in range(start, end+1) if is_prime(n)]
-    return primes
+    def primes(nums):
+        """"""
+        primes = []
+        for n in nums:
+            if is_prime(n):
+                primes.append(n)
+
+        return primes
+
+    moves = primes(nums)
+
+    return "Ben"
